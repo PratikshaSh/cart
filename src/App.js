de80fbffd83a5aecbaf2ce6b7132f8556a4e7cea
PratikshaSh <pratiksha8520@gmail.com>
2021-03-31 03:45:10 +0530

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Cart from './Cart';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar/>
         <Cart/>
       </header>
     </div>
