4438b170b1eafc3218420c3c47f2ac3fed20cabc
PratikshaSh <pratiksha8520@gmail.com>
2021-03-31 21:07:21 +0530

    add images

--- src/App.js
+++ src/App.js
@@ -11,21 +11,21 @@ class App extends React.Component{
             price: 99,
             title: 'Watch',
             qty: 1,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
             id: 1
         },
         {
             price: 999,
             title: 'Mobile Phone',
             qty: 10,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1580910051074-3eb694886505?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=701&q=80',
             id: 2
         },
         { 
             price: 9999,
             title: 'Laptop',
             qty: 4,
-            img: '',
+            img: 'https://images.unsplash.com/photo-1593642632823-8f785ba67e45?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80',
             id: 3
         }
        ]
